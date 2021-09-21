191bc278696177d94f9e06f04905a7ef069291ce
Rohail Rafique <rohailrafique@gmail.com>
2021-09-21 09:26:11 -0400

    commit tree dca1c0a65f664cad66a86d23bb6b500c529a1bc3

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+// // Write your code here
+// describe('index.js', function() {
+//     describe('multiply', function() {
+//       it("is an equation whose multiplied results will equal 62", function() {
+//         expect(num1).to.be.a('number')
+//         expect(num2).to.be.a('number')
+//         expect(multiply).to.eq(62)
+//       });
+
+const num1 = 31;
+const num2= 2;
+const multiply = (num1 * num2);
+
+// describe('random', function() {
+//     it("generates a random integer greater than 0", function() {
+//       expect(Number.isInteger(random)).to.be.true
+//       expect(random).to.not.eq(0)
+
+Math.floor(Math.random() * 10);
+
